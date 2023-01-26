873dce519c3f11ef7c1fcd78a25244406a199be7
Autumn Williams <brandonhdyc@gmail.com>
2023-01-25 23:34:32 -0500

    switching from HTTP to raw text over TCP

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const http = require('http')
 const fs = require('fs')
 const config = JSON.parse(fs.readFileSync("config.json"))
+const net = require('net');
+const server = net.createServer(function(socket) {
+	socket.write('Echo server\r\n');
+	socket.pipe(socket);
+});
+
 console.log("Starting OpenPOS Server")
 console.log("Version " + config.version)
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Pong!')
-    console.log("Ping")
-}).listen(8080);
+server.listen(8080, '127.0.0.1');
