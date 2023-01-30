9d7e0e0131bd2fb2ae00c77dbdb71c885912b518
Autumn Williams <brandonhdyc@gmail.com>
2023-01-30 14:19:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const fs = require('fs')
 const config = JSON.parse(fs.readFileSync("config.json"))
 const net = require('net');
 const server = net.createServer(function(socket) {
-	socket.write('Echo server\r\n');
+	
+	socket.write('0.1.0');
 	socket.pipe(socket);
-});
 
+});
+server.on('data', function(data) {
+	console.log('Received: ' + data);
+});
 console.log("Starting OpenPOS Server")
 console.log("Version " + config.version)
 server.listen(8080, '127.0.0.1');
